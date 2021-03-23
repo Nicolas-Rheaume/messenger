2126494fefe4c615f083ffb0e109bee7ec6af691
Nick-Rheaume <nick.rheaume@gmail.com>
2021-03-23 15:27:14 -0400

    CD/CI v14

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,10 @@ export const environment = {
   production: true,
   socketioConfig: {
     url: 'https://nickrheaume.ca/messenger/socket.io', 
-    path: '/messenger/socket.io',
     options: {
+      transports: ['websocket'], 
+      allowUpgrades : true,
+      path: '/messenger/socket.io',
       withCredentials: true,
       extraHeaders: {
         "nr-portfolio": "nr-portfolio"
